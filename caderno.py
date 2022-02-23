b456a982f62de170d6ff128fbd2b4b05543ee4d4
italo-baciliere <italoluciano@gmail.com>
2022-02-23 16:31:52 -0300

    Manipulando Strings

---
+++ caderno.py
@@ -0,0 +1,47 @@
+'''
+
+Python documentation:
+    https://docs.python.org/3/index.html
+
+Pesquisar sobre
+    if(__name__ == "__main__"):
+
+bool
+    representa verdadeiro ou falso no Python
+
+built-in
+    são funções que não precisa ser importadas,
+    pois estão disponíveis para uso automaticamente.
+
+"Truth Value Testing"
+    Função relacionada com o tipo bool.
+    Decidir quando um valor é considerado True ou False.
+
+    >>> bool(0)
+    False
+    >>> bool("")
+    False
+    >>> bool(None)
+    False
+    >>> bool(1)
+    True
+    >>> bool(-100)
+    True
+    >>> bool(13.5)
+    True
+    >>> bool("teste")
+    True
+    >>> bool(True)
+    True
+
+find()
+    A função find encontra a primeira ocorrência
+    do texto que estamos procurando e devolve o índice.
+
+Strings
+    >>> palavra = "alura"
+    >>> palavra.upper()
+    >>> print(palavra) #qual é o resultado?
+    
+    Strings são imutáveis, são sequências imutáveis!
+'''
