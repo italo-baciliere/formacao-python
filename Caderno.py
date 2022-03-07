fd8e26514aa60b200175efb745aeb6878ab7e82b
italo-baciliere <italoluciano57@gmail.com>
2022-03-07 09:41:46 -0300

    Primeiro Commit

---
+++ Caderno.py
@@ -0,0 +1,13 @@
+'''
+FAÇA ESSE CURSO DE PYTHON E:
+
+    * Aprenda o paradigma OO com Python 3
+    * Aplique Orientação a objetos com atributos, métodos e classes
+    * Veja como usar propriedades
+    * Entenda encapsulamento e coesão
+    * Saiba quando deixar o seu método privado ou público
+
+AULAS
+
+    
+'''
